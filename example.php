c47b503c715e1ae26619245607c925592b46d163
Faiz A. Farooqui <faizz.af@gmail.com>
2016-12-29 16:49:20 +0530

    Create example.php

---
+++ example.php
@@ -0,0 +1,15 @@
+<?php
+include './ProxyChecker.php';
+
+$proxies = [
+				'XXX.XXX.XXX.XXX:XXXX,username:password,Socks4',
+                'XXX.XXX.XXX.XXX:XXXX,username:password,Socks5',
+                'XXX.XXX.XXX.XXX:XXXX'
+			];
+
+$proxyChecker = new ProxyChecker('http://www.google.com/');
+$results = $proxyChecker->checkProxies($proxies);
+
+echo "<pre>";
+print_r($results);
+echo "</pre>";
